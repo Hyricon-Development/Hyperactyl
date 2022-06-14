3cb833b13d1c0ec1c809a47034daafd495515d25
RianOP <87343593+RianOP@users.noreply.github.com>
2022-06-13 20:25:07 -0400

    progress for devs

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,14 +1,12 @@
-const express = require('express')
-
 module.exports.load = async function (app) {
 
     app.get('/', async (req, res) => {
         if (!req.session.data) return res.redirect('/auth/login')
-        res.sendFile('../themes/pages/index.html')
+        return res.sendFile('../themes/pages/index.html')
     })
 
     app.get('/auth/login', async (req, res) => {
         if (req.session.data) return res.redirect('/')
-        res.sendFile('../themes/pages/auth.html')
+        return res.sendFile(__dirname, '../themes/pages', 'auth.html')
     })
 }
